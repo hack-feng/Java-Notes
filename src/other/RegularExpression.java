4a9ce179a20554146135a4c0356693ed3b30cd00
hack-feng <1150640979@qq.com>
2019-09-20 17:39:57 +0800

    add 正则.

---
+++ src/other/RegularExpression.java
@@ -0,0 +1,31 @@
+package other;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 正则表达式
+ */
+public class RegularExpression {
+
+    public static void main(String[] args) {
+        String str = "hello world 111";
+
+        Pattern p = Pattern.compile("\\b\\w+\\b");
+
+        Matcher m = p.matcher(str);
+        while (m.find()){
+            //hello-0-5
+            //world-6-11
+            System.out.println(m.group() + "-" + m.start() + "-" + m.end());
+        }
+
+
+        String i = "123";
+        Pattern p1 = Pattern.compile("^\\d+$");
+        Matcher m1 = p1.matcher(i);
+        while(m1.find()){
+            System.out.println(m1.group());
+        }
+    }
+}
