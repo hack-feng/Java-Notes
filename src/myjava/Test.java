cffde3f8ab1036f382f6ad4b565d60973473d718
hack-feng <1150640979@qq.com>
2020-04-26 12:10:22 +0800

    修改文档

--- src/myjava/Test.java
+++ src/myjava/Test.java
@@ -1,5 +1,6 @@
 package myjava;
 
+import java.text.ParseException;
 import java.util.*;
 
 /**
@@ -7,35 +8,11 @@ import java.util.*;
  */
 public class Test {
 
-    public static void main(String[] args) {
-        int [][] test = new int [][]{{2, 3}, {1, 6}, {8, 10}, {15, 18}};
-        Arrays.sort(test, (o1, o2) -> {
-            if(o1[0] == o2[0]){
-                return o1[1] - o2[1];
-            }else{
-                return o1[0] - o2[0];
-            }
-        });
+    public static void main(String[] args) throws ParseException {
 
-        List<int[]> list = new ArrayList<>();
-        for (int[] array : test){
-            if(list.isEmpty()){
-                list.add(array);
-            }else{
-                int [] flag = list.get(list.size() - 1);
+        Map<String, Object> map = new HashMap<>();
+        map.put("aaa", 123);
+        System.out.println(map);
 
-                if(array[0] > flag[1]){
-                    list.add(array);
-                }else{
-                    if(flag[1] < array[1]){
-                        list.remove(flag);
-                        list.add(new int[]{flag[0], array[1]});
-                    }
-                }
-            }
-        }
-
-        int result [][] = list.toArray(new int[list.size()][2]);
-        System.out.println(Arrays.deepToString(result));
     }
 }
